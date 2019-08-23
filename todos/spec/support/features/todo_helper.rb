c8df3830f9c3e249e0abd3a94adc42d06531d594
Amanda <amanda.macgregor13@gmail.com>
2019-08-23 18:12:32 +0000

    some refactors don't work with my versions (or something) - namely, module Features.  Otherwise, tests are refactored and moving on to final video

---
+++ todos/spec/support/features/todo_helper.rb
@@ -0,0 +1,9 @@
+# this refactor just doesn't play nice with my version of things.  Features is a no-go pretty much across the board.
+
+# module Features
+#     def create_todo(todo_title)
+#         click_on "Add a new todo"
+#         fill_in "Title", with: todo_title
+#         click_on "Submit"
+#     end   
+# end
