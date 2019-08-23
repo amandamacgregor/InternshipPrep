f1d6ebe5760dafbb33a8b8c6370f8027cd10656b
Amanda <amanda.macgregor13@gmail.com>
2019-08-23 18:51:17 +0000

    can't complete the video due to outdated video instructions - tests are now failing due to prior refactorization

---
+++ todos/spec/features/user_marks_todo_incomplete_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+feature "User marks todo incomplete" do
+    scenario "successfully" do
+        sign_in
+        
+        create_todo "Buy milk"
+
+        click_on "Mark complete"
+        click_on "Mark incomplete"
+                
+        expect(page).not_to have_css ".todos li.completed", text: "Buy milk"
+        expect(page).to have_css ".todos li.completed", text: "Buy milk"
+        # expect(page).not_to display_completed_todo "Buy milk"
+        # expect(page).to display_todo "Buy milk"
+    end
+    
+    def create_todo(todo_title)
+        click_on "Add a new todo"
+        fill_in "Title", with: todo_title
+        click_on "Submit"
+    end    
+end
